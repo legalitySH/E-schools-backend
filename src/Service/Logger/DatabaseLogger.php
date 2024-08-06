de9422594ae38d8eb0dc1c9304d8a3c4fc34565b
alexei <rpsher@bk.ru>
2024-08-06 14:14:59 +0300

    Service to log server errors

--- src/Service/Logger/DatabaseLogger.php
+++ src/Service/Logger/DatabaseLogger.php
@@ -6,53 +6,11 @@ namespace App\Service\Logger;
 
 use App\Entity\LogEntry;
 use Doctrine\ORM\EntityManagerInterface;
-use Psr\Log\LoggerInterface;
-use Psr\Log\LogLevel;
 
-class DatabaseLogger implements LoggerInterface
+class DatabaseLogger extends AbstractLogger
 {
     public function __construct(private readonly EntityManagerInterface $entityManager)
     {
-    }
-
-    public function emergency($message, array $context = []): void
-    {
-        $this->log(LogLevel::EMERGENCY, $message, $context);
-    }
-
-    public function alert($message, array $context = []): void
-    {
-        $this->log(LogLevel::ALERT, $message, $context);
-    }
-
-    public function critical($message, array $context = []): void
-    {
-        $this->log(LogLevel::CRITICAL, $message, $context);
-    }
-
-    public function error($message, array $context = []): void
-    {
-        $this->log(LogLevel::ERROR, $message, $context);
-    }
-
-    public function warning($message, array $context = []): void
-    {
-        $this->log(LogLevel::WARNING, $message, $context);
-    }
-
-    public function notice($message, array $context = []): void
-    {
-        $this->log(LogLevel::NOTICE, $message, $context);
-    }
-
-    public function info($message, array $context = []): void
-    {
-        $this->log(LogLevel::INFO, $message, $context);
-    }
-
-    public function debug($message, array $context = []): void
-    {
-        $this->log(LogLevel::DEBUG, $message, $context);
     }
 
     public function log($level, $message, array $context = array()): void
