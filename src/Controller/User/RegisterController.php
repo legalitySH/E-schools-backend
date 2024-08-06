032967b27b1ce12c5bb6081b4f1481d679b039d2
alexei <rpsher@bk.ru>
2024-08-06 14:15:47 +0300

    Fixed RegisterController.php(Logic separation)

--- src/Controller/User/RegisterController.php
+++ src/Controller/User/RegisterController.php
@@ -5,14 +5,12 @@ declare(strict_types=1);
 namespace App\Controller\User;
 
 use App\Entity\User;
-use App\Form\UserRegistrationForm;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RegisterController extends AbstractController
@@ -27,37 +25,25 @@ class RegisterController extends AbstractController
     #[Route('/api/register', methods: ['POST'])]
     public function register(Request $request, FormFactoryInterface $formFactory): Response
     {
-        $user = new User();
-        $form = $formFactory->create(UserRegistrationForm::class, $user);
+        $requestData = json_decode($request->getContent(), true);
 
-        $form->submit(json_decode($request->getContent(), true));
+        $user = (new User())
+            ->setEmail($requestData['email'])
+            ->setUsername($requestData['username']);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $hashedPassword = $this->passwordHasher->hashPassword($user, $user->getPlainPassword());
-            $user->eraseCredentials();
-            $user->setPassword($hashedPassword);
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $requestData['plain_password']);
+        $user->eraseCredentials();
+        $user->setPassword($hashedPassword);
 
-            try{
-                $this->entityManager->persist($user);
-                $this->entityManager->flush();
-            }
-            catch (\Exception $exception)
-            {
-                return $this->json(['error' => 'User already exists !'], Response::HTTP_BAD_REQUEST);
-            }
-            return new Response('Please log in to your account !', Response::HTTP_OK);
+        try{
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
+        catch (\Exception $exception)
+        {
+            return $this->json(['error' => 'User already exists !'], Response::HTTP_BAD_REQUEST);
         }
 
-        $errors = $form->getErrors(true, true);
-        $errorMessages = [];
-
-        foreach ($errors as $error) {
-            $errorMessages[] = $error->getMessage();
-        }
-
-        return $this->json([
-            'error' => $errorMessages,
-        ], Response::HTTP_BAD_REQUEST);
+        return new Response('Please log in to your account!', Response::HTTP_OK);
     }
-
 }
