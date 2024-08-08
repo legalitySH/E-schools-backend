bbd63218a9fd87644176f3666e7b607c28b66975
alexei <rpsher@bk.ru>
2024-08-08 14:50:11 +0300

    Configured new bundle for OAuth 2

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
 ];
