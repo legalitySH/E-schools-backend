9f3156db70fc18d9c0a4992d7ad2b33210168adc
alexei <rpsher@bk.ru>
2024-08-10 15:36:18 +0300

    fix: catching application exceptions instead http

--- src/Controller/Application/ApplicationReviewController.php
+++ src/Controller/Application/ApplicationReviewController.php
@@ -5,27 +5,33 @@ declare(strict_types=1);
 namespace App\Controller\Application;
 
 use App\Service\Application\Api\ApplicationReviewServiceInterface;
+use App\Service\Application\ApplicationReviewService;
+use App\Service\Application\Exception\ApplicationApproveException;
+use App\Service\Application\Exception\ApplicationNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ApplicationReviewController extends AbstractController
 {
-    function __construct(private readonly ApplicationReviewServiceInterface $reviewService)
+    /**
+     * @param ApplicationReviewService $reviewService
+     */
+    public function __construct(private readonly ApplicationReviewServiceInterface $reviewService)
     {
 
     }
+
     #[Route('/api/application/approve/id={id}')]
     public function approveApplication(int $id): Response
     {
         try {
             $this->reviewService->approve($id);
         }
-        catch (HttpException $exception){
-            return $this->json(['error' => $exception->getMessage()], $exception->getStatusCode());
-        } catch (\Exception $exception){
-            return $this->json(['error' => $exception->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        catch (ApplicationApproveException $exception) {
+            return new Response($exception->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        } catch (ApplicationNotFoundException $exception) {
+            return new Response($exception->getMessage(), Response::HTTP_NOT_FOUND);
         }
 
         return new Response("Application with id $id was approved", Response::HTTP_OK);
@@ -37,10 +43,10 @@ final class ApplicationReviewController extends AbstractController
         try {
             $this->reviewService->reject($id);
         }
-        catch (HttpException $exception){
-            return $this->json(['error' => $exception->getMessage()], $exception->getStatusCode());
-        } catch (\Exception $exception){
-            return $this->json(['error' => $exception->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        catch (ApplicationApproveException $exception) {
+            return new Response($exception->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        } catch (ApplicationNotFoundException $exception) {
+            return new Response($exception->getMessage(), Response::HTTP_NOT_FOUND);
         }
 
         return new Response("Application with id $id was rejected", Response::HTTP_OK);
