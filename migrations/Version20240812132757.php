1e5a31cc9a62ed9ca2b5c97f628c640576b93225
alexei <rpsher@bk.ru>
2024-08-12 16:54:33 +0300

    feat: migrate new table with institutions types

--- migrations/Version20240812132757.php
+++ migrations/Version20240812132757.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240809112728 extends AbstractMigration
+final class Version20240812132757 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,15 @@ final class Version20240809112728 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE educational_institution ADD email VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE SEQUENCE institution_type_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE institution_type (id INT NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE educational_institution DROP email');
+        $this->addSql('DROP SEQUENCE institution_type_id_seq CASCADE');
+        $this->addSql('DROP TABLE institution_type');
     }
 }
