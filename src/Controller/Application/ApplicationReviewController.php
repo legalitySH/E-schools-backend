d6d507f24a3ae7cf94f4ed50bccea8c8fe93b5cf
alexei <rpsher@bk.ru>
2024-08-16 18:07:21 +0300

    refactor: change prefix route name

--- src/Controller/Application/ApplicationReviewController.php
+++ src/Controller/Application/ApplicationReviewController.php
@@ -12,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/api/application', name: 'application_', methods: ['PATCH'])]
+#[Route('/api/applications', name: 'application_', methods: ['PATCH'])]
 final class ApplicationReviewController extends AbstractController
 {
     /** @param ApplicationReviewService $reviewService */
