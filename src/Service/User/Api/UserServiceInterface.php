8fcf8279659a8aea1e00439a9a6aab351466c0cf
alexei <rpsher@bk.ru>
2024-08-16 17:52:52 +0300

    feat(user): added new method to change user avatar

--- src/Service/User/Api/UserServiceInterface.php
+++ src/Service/User/Api/UserServiceInterface.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace App\Service\User\Api;
 
 use App\Dto\User\RegisterUserDto;
+use App\Entity\User;
 
 interface UserServiceInterface
 {
     public function register(RegisterUserDto $dto): void;
+
+    public function setAvatar(User $user, string $avatarUrl): void;
 }
