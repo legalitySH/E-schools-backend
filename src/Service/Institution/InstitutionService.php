f2ccd42bd0d8f65c4906cd61751207c063bfc7a9
alexei <rpsher@bk.ru>
2024-08-28 10:44:47 +0300

    refactor(institution): changed base interface on specific

--- src/Service/Institution/InstitutionService.php
+++ src/Service/Institution/InstitutionService.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 namespace App\Service\Institution;
 
-use App\Repository\Api\RepositoryInterface;
+use App\Repository\Api\BaseRepositoryInterface;
+use App\Repository\Api\InstitutionTypeRepositoryInterface;
 use App\Repository\InstitutionTypeRepository;
 use App\Service\Institution\Api\InstitutionServiceInterface;
 
 final class InstitutionService implements InstitutionServiceInterface
 {
-    /** @param InstitutionTypeRepository $institutionTypesRepository */
     public function __construct(
-        private readonly RepositoryInterface $institutionTypesRepository,
+        private readonly InstitutionTypeRepositoryInterface $institutionTypesRepository,
     ) {
     }
 
